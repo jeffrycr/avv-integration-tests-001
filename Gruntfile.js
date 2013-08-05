92feefc654f54e98f66bf4758d37eaaa85e9e047
andresr <andres01cr@gmail.com>
2013-08-05 11:45:32 -0600

    minor changing files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,44 @@
-module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
-};
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-nodeunit');
+grunt.loadNpmTasks('grunt-ghost');
+grunt.registerTask('test', ['jshint', 'nodeunit', 'connect', 'ghost']);
+jshint: {
+	options: {
+		jshintrc: '.jshintrc'
+	},
+	files: [
+		'Gruntfile.js',
+		'package.json',
+		'source/**/*.js',
+		'<%= nodeunit.tests %>'
+	]
+},
+nodeunit: {
+	tests: ['tests/nodeunit/*_test.js']
+},
+connect: {
+	www: {
+		options: {
+			base: 'source',
+			port: 4545
+		}
+	}
+},
+ghost: {
+	test: {
+		files: [{
+			src: ['tests/ghost/*_test.js']
+		}]
+	},
+	options: {
+		args: {
+			baseUrl: 'http://localhost:' +
+				'<%= connect.www.options.port %>/'
+		},
+		direct: false,
+		logLevel: 'error',
+		printCommand: false,
+		printFilePaths: true
+	}
+}
