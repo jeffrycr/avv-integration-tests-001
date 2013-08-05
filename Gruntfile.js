8aa852487288d9361c1722955e57cbc07cf17e50
Jeffry Araya <jaraya@yahoo-inc.com>
2013-08-05 13:17:41 -0600

    minor change

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,11 @@
-grunt.loadNpmTasks('grunt-contrib-connect');
-grunt.loadNpmTasks('grunt-contrib-jshint');
-grunt.loadNpmTasks('grunt-contrib-nodeunit');
-grunt.loadNpmTasks('grunt-ghost');
-grunt.registerTask('test', ['jshint', 'nodeunit', 'connect', 'ghost']);
-jshint: {
-	options: {
-		jshintrc: '.jshintrc'
-	},
-	files: [
-		'Gruntfile.js',
-		'package.json',
-		'source/**/*.js',
-		'<%= nodeunit.tests %>'
-	]
-},
-nodeunit: {
-	tests: ['tests/nodeunit/*_test.js']
-},
-connect: {
-	www: {
-		options: {
-			base: 'source',
-			port: 4545
-		}
-	}
-},
-ghost: {
-	test: {
-		files: [{
-			src: ['tests/ghost/*_test.js']
-		}]
-	},
-	options: {
-		args: {
-			baseUrl: 'http://localhost:' +
-				'<%= connect.www.options.port %>/'
-		},
-		direct: false,
-		logLevel: 'error',
-		printCommand: false,
-		printFilePaths: true
-	}
-}
+'use strict';
+module.exports = function (grunt) {
+  // Project configuration.
+    grunt.initConfig({
+        // Configuration to be run (and then tested).
+        casperjs: {
+            files: ['test/casperjs.js']
+        }
+    });
+    grunt.loadNpmTasks('grunt-casperjs');
+};
