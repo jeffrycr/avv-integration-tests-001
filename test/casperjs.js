5712b43bde581114af37e78d9cc1745622ea8f05
andresr <andres01cr@gmail.com>
2013-08-06 09:02:16 -0600

    minor change test file

--- test/casperjs.js
+++ test/casperjs.js
@@ -5,10 +5,11 @@ casper.start('http://www.google.com/', function () {
     this.fill('form[action="/search"]', {
         q: 'us news'
     }, true);
+	   this.test.assertSelectorHasText('.f cite','news.yahoo.com/us/');
 });
 
 casper.then(function () {
-      test.assertSelectorHasText('.f cite','news.yahoo.com/us/');
+  // this.test.assertSelectorHasText('.f cite','news.yahoo.com/us/');
 	//  var links = document.querySelectorAll('.f cite');
 	//     this.echo(this.fetchText('.f cite'));
 	//  this.echo(links);
