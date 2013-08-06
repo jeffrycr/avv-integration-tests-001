687fe29f0d2f63ff88de8f4e2db6dc715c69666c
andresr <andres01cr@gmail.com>
2013-08-06 08:56:37 -0600

    minor change test file

--- test/casperjs.js
+++ test/casperjs.js
@@ -1,13 +1,5 @@
 var casper = require('casper').create();
 
-casper.test.begin('assertSelectorHasText() tests', 1, function(test) {
-    casper.start('http://google.com/', function() {
-        test.assertSelectorHasText('.f cite', 'news.yahoo.com/us/');
-    }).run(function() {
-        test.done();
-    });
-});
-/*
 casper.start('http://www.google.com/', function () {
     this.test.assertExists('form[action="/search"]', 'main form is found');
     this.fill('form[action="/search"]', {
@@ -17,11 +9,11 @@ casper.start('http://www.google.com/', function () {
 
 casper.then(function () {
       test.assertSelectorHasText('.f cite','news.yahoo.com/us/');
-	  var links = document.querySelectorAll('.f cite');
-	     this.echo(this.fetchText('.f cite'));
-	  this.echo(links);
+	//  var links = document.querySelectorAll('.f cite');
+	//     this.echo(this.fetchText('.f cite'));
+	//  this.echo(links);
 });
 
 casper.run(function () {
     this.test.renderResults(true);
-});*/
+});
