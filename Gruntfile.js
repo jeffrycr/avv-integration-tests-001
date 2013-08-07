5e2937874b30fbe7765950ae37aaf6945b597a33
maumunoz <maumunoz@gmail.com>
2013-08-07 10:48:05 -0600

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
         // Configuration to be run (and then tested).
         casperjs: {
-            files: ['test/casperjs.js']
+            files: ['test/**/*.js']
         }
     });
     grunt.loadNpmTasks('grunt-casperjs');
