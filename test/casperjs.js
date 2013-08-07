80f9641aa54cfce60d54107613731dbbd0b2f29b
andresr <andres01cr@gmail.com>
2013-08-07 09:41:37 -0600

    minor change test file

--- test/casperjs.js
+++ test/casperjs.js
@@ -5,19 +5,20 @@ casper.start('http://www.google.com/', function () {
     this.fill('form[action="/search"]', {
         q: 'us news'
     }, true);
-	var cites = document.querySelectorAll('.f cite'),
+});
+casper.then(function() {
+var cites = document.querySelectorAll('.f cite'),
 		found = false;
-	this.echo(this.fetchText('.f cite'));
+	
 	for (i = 0; i < cites.length && !found; i += 1) {
 	
-	  if (cites[i].textContent == "news.yahoo.com/us/") {
+	  if (cites[i].textContent === "news.yahoo.com/us/") {
 		found = true;
 	  }
 	}
 	this.echo(found);
 	this.test.assert(found, 'Found Yahoo News');  
 });
-
 casper.run(function () {
     this.test.renderResults(true);
 });
