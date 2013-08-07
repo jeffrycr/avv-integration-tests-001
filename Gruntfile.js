c845e9f00ed275834aaf6e694592c60c777e2c8c
Jeffry Araya <jaraya@yahoo-inc.com>
2013-08-07 15:55:16 -0600

    Including new files for karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,34 @@
+/*globals JASMINE, JASMINE_ADAPTER, MOCHA, MOCHA_ADAPTER, QUNIT, QUNIT_ADAPTER,*/
 'use strict';
 module.exports = function (grunt) {
   // Project configuration.
     grunt.initConfig({
         // Configuration to be run (and then tested).
         ghost: {
-        	integration: {
-        		filesSrc: ['integration-test/**/*.js']
-        	}
+            integration: {
+                filesSrc: ['integration-test/**/*.js']
+            }
+        },
+        karma: {
+            options: {
+                files: [
+                    JASMINE,
+                    JASMINE_ADAPTER,
+                    MOCHA,
+                    MOCHA_ADAPTER,
+                    QUNIT,
+                    QUNIT_ADAPTER
+                ],
+                runnerPort: 9999,
+                browsers: ['PhantomJS']
+            },
+            unit: {
+                singleRun: true
+            }
         }
     });
     grunt.loadNpmTasks('grunt-ghost');
+    grunt.loadNpmTasks('grunt-karma');
     grunt.registerTask('test', ['ghost']);
+    grunt.registerTask('test', ['karma:unit']);
 };
