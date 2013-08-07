11a8ebf015490e368435b157aad4ff4914d3134c
Mauricio Munoz <mmunoz@yahoo-inc.com>
2013-08-07 13:29:19 -0600

    Fixing Google Search Results Tests for US News

--- test/casperjs.js
+++ test/casperjs.js
@@ -1,37 +1,15 @@
-var casper = require('casper').create();
-
-casper.test.begin('Verify Yahoo! News in Google results for "us news"', function suite(test) {
+casper.test.begin('Verify Yahoo! News in Google results for "us news"', function (test) {
     casper.start('http://www.google.com/', function () {
-    	test.assertExists('form[action="/search"]', 'main form is found');
-    	this.fill('form[action="/search"]', {
+        test.assertExists('form[action="/search"]', 'main form is found');
+        this.fill('form[action="/search"]', {
             q: 'us news'
-    	}, true);
+        }, true);
     });
     casper.then(function () {
-    	test.assertExists('ol#rso');
+        test.assertExist('div#search');
+        test.assertSelectorHasText('div#search cite', 'news.yahoo.com');
     });
     casper.run(function () {
-    	test.done();
+        test.done();
     });
 });
-
-    
-/*
-});
-casper.then(function() {
-var cites = document.querySelectorAll('.f cite'),
-		found = false,
-		i;
-	this.echo(cites[7].textContent);
-	for (i = 0; i < cites.length && !found; i += 1) {
-	this.echo(cites[i].textContent);
-	  if (cites[i].textContent === "news.yahoo.com/us/") {
-		found = true;
-	  }
-	}
-	this.echo(found);
-	this.test.assert(found, 'Found Yahoo News');  
-});
-casper.run(function () {
-    this.test.renderResults(true);
-});*/
