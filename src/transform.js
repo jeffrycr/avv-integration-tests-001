122009b99575240fcd2aef2164fbcd44eb41914e
Mauricio Munoz <mmunoz@yahoo-inc.com>
2013-08-09 13:59:26 -0600

    Adding transform.js

---
+++ src/transform.js
@@ -0,0 +1,18 @@
+/*global _ */
+/*exported dataTransform */
+function dataTransform(data) {
+    'use strict';
+    var result;
+
+    if (!_.isObject(data) || _.keys(data) === 0) {
+        result = {
+            error: 'No Keys Founds'
+        };
+    } else {
+        result = {
+            success: true,
+            name: _.pluck(data, 'name')
+        };
+    }
+    return result;
+}
